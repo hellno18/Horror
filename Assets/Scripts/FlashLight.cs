4f25f24af7b5233c1d30da860372fdfec4050bf4
hellno18 <40555371+hellno18@users.noreply.github.com>
2019-10-11 12:32:31 +0900

    Commit

---
+++ Assets/Scripts/FlashLight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+    bool isLight;
+    GameObject hand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isLight = false;
+        hand = GameObject.Find("Hand").GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Flashlight")!=0)
+        {
+            isLight = true;
+            hand.SetActive(false);
+        }
+    }
+}
