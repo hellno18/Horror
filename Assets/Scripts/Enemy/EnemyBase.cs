b459a994815b22d132dd664611dc50bad62749bf
Sugi, pls stop smoking weed <>
2019-10-17 15:55:37 +0900

    EnemyBaseCode Added

---
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBase : MonoBehaviour
+{
+    protected int enemyHealth = 20;
+    protected int attack;
+    protected NavMeshAgent navMesh;
+    protected Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMesh = this.transform.GetComponent<NavMeshAgent>();
+        animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ComputeVelocity();   
+    }
+
+
+    protected virtual void ComputeVelocity()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemySlender.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySlender : EnemyBase
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    protected override void ComputeVelocity()
+    {
+     
+    }
+
+}
