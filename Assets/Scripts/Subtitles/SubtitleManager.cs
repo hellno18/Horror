03eed57d12529aa0eb50b06a699116048b7ab997
hellno18 <40555371+hellno18@users.noreply.github.com>
2019-10-18 12:32:01 +0900

    Subtitles Added

---
+++ Assets/Scripts/Subtitles/SubtitleManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class SubtitleManager : MonoBehaviour
+{
+  void Start()
+  {
+    // Typically Begin would be called from the same place that starts the video
+    StartCoroutine(FindObjectOfType<SubtitleDisplayer>().Begin());
+  }
+}
