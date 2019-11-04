d188e96352cb7deddc2d3a9aab7352f4215f2c45
Michael Jack <michaeljack255@gmail.com>
2019-11-04 18:05:05 +0900

    Cube Features

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    bool hold;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            hold = true;
+        }
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            hold = false;
+        }
+        if (hold == false)
+        {
+            this.transform.parent = null;
+            //Debug.Log("released");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && hold)
+        {
+            this.transform.parent = other.transform;
+            //Debug.Log("holding");
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -140,4 +140,5 @@ public class PlayerController : PlayerBase
             keyCount =value;
         }
     }
+
 }
