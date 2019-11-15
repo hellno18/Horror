2fa804dd31d2f1f345d69348e0de86060f9e907f
hellno18 <40555371+hellno18@users.noreply.github.com>
2019-11-15 15:14:37 +0900

    Updated

--- Assets/Scripts/Item/Elevator.cs
+++ Assets/Scripts/Item/Elevator.cs
@@ -8,14 +8,18 @@ public class Elevator : MonoBehaviour
     ElectroPanel electroPanel;
     Transform rightDoor;
     Transform leftDoor;
+    Transform boxElevator;
     bool isInteract;
     bool isOpen;
+    bool isReady;
     private float openTime = 0;
     private float openSpeed = 2.0f; //Increasing this value will make the door open faster
     private float currentLeftX;
     private float defaultLeftX;
     private float currentRightX;
     private float defaultRightX;
+    private float ptk_Elevator_Y;
+    private float ptk_Elevator_Default_Y;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,11 +32,16 @@ public class Elevator : MonoBehaviour
         leftDoor = this.transform.Find("PTK_Elevator/Left").GetComponent<Transform>();
         //reference to right door
         rightDoor = this.transform.Find("PTK_Elevator/Right").GetComponent<Transform>();
+        //reference to box elevator
+        boxElevator = this.transform.Find("PTK_Elevator").GetComponent<Transform>();
         isOpen = false;
         currentLeftX = leftDoor.position.z;
         defaultLeftX = leftDoor.position.z;
         currentRightX = rightDoor.position.z;
         defaultRightX = rightDoor.position.z;
+        ptk_Elevator_Y = boxElevator.position.y;
+        ptk_Elevator_Default_Y = boxElevator.position.y;
+
     }
 
     // Update is called once per frame
@@ -49,16 +58,47 @@ public class Elevator : MonoBehaviour
         rightDoor.transform.position = new Vector3(rightDoor.position.x, rightDoor.transform.position.y, Mathf.Lerp(currentRightX,
             defaultRightX + (isOpen ? -1.9f : 0),
             openTime));
+        //MoveUp;
+        boxElevator.position = new Vector3(boxElevator.position.x,
+           Mathf.Lerp(ptk_Elevator_Y, ptk_Elevator_Default_Y + (isReady ? 4 : 0), openTime), boxElevator.position.z);
+
         if (electroPanel.GetIsElevator)
         {
             //electro true
             if (Input.GetButtonDown("Interact") && isInteract)
             {
-                isOpen = !isOpen;
-                openTime = 0;
-                currentLeftX = leftDoor.position.z;
-                currentRightX = rightDoor.position.z;
+                Move();
             }
+        }
+    }
+
+
+    public void Move()
+    {
+        isOpen = !isOpen;
+        openTime = 0;
+        currentLeftX = leftDoor.position.z;
+        currentRightX = rightDoor.position.z;
+    }
+
+    public void MoveUp()
+    {
+        isReady = !isReady;
+        openTime = 0;
+        ptk_Elevator_Y = boxElevator.position.y;
+        if(isReady)
+        isOpen = !isOpen;
+    }
+
+    public bool IsReady
+    {
+        get
+        {
+           return isReady;
+        }
+        set
+        {
+            isReady = value;
         }
     }
 
---
+++ Assets/Scripts/Item/ElevatorUp.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorUp : MonoBehaviour
+{
+    Elevator elevator;
+    ElectroPanel electroPanel;
+    bool isInteract;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        elevator = GameObject.Find("PTK_Elevator_3Floors").GetComponent<Elevator>();
+        //get component electropanel
+        electroPanel = GameObject.Find("ElectoPanel_Elevator/DetectElectro")
+            .GetComponent<ElectroPanel>();
+        isInteract = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         //electro true
+       if (Input.GetButtonDown("Interact") && isInteract)
+       {
+            if (!elevator.IsReady)
+            {
+                StartCoroutine(ElevatorCoroutine());
+            }
+           
+           
+        }
+       
+    }
+
+    IEnumerator ElevatorCoroutine()
+    {
+        elevator.Move();
+        yield return new WaitForSeconds(2f);
+        elevator.MoveUp();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")){
+            isInteract = true;          
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isInteract = false;
+        }
+    }
+}
