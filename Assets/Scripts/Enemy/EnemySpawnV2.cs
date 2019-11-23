8b6be30b95333f7a4a4e91369538285bd6d45111
Michael Jack <michaeljack255@gmail.com>
2019-11-23 23:12:34 +0900

    Fixed Bug Slender Spawn & spawnerV2

---
+++ Assets/Scripts/Enemy/EnemySpawnV2.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnV2 : MonoBehaviour
+{
+    [SerializeField] private GameObject enemy;                // The enemy prefab to be spawned.
+    private PlayerController playerHealth;       // Reference to the player's heatlh.
+    private Transform player;            // The position that that camera will be following.
+    private FlashLight flashlight;       //reference to the player flashlight
+    private float spawnTime = 5f;            // How long between each spawn.
+    private float spawnDistance = 13f;    //reference betweem the player and enemy
+
+    private void Start()
+    {
+        /******************
+         * GET COMPONENT 
+         ******************/
+        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        flashlight = player.GetComponentInChildren<FlashLight>();
+
+        //gameobject children set into false
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+
+        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+        InvokeRepeating("Spawn", Time.time, spawnTime);
+
+    }
+
+    private void Spawn()
+    {
+        // If the player has no health left...
+        if (playerHealth.GetHealth <= 0f)
+        {
+            // ... exit the function.
+            return;
+        }
+
+        // collect the children that are close.
+        List<Transform> near = new List<Transform>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform spawnPoint = transform.GetChild(i);
+            //debugger
+            //print(Vector3.Distance(player.transform.position, spawnPoint.position)+"xx"+ transform.GetChild(i));
+            if (Vector3.Distance(player.transform.position, spawnPoint.position) <= spawnDistance)
+            {
+                near.Add(spawnPoint);
+            }
+        }
+
+        if (near.Count > 0)
+        {
+            // Find a random index between zero and one less than the number of spawn points.
+            int spawnPointIndex = Random.Range(0, near.Count);
+
+            //while player not used flashlight, enemy will come from detect spot(near player) 
+            if (!flashlight.GetIsLight)
+            {
+                // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+                GameObject instance = (GameObject)Instantiate(enemy, near[spawnPointIndex].position, near[spawnPointIndex].rotation);
+                instance.transform.Rotate(Vector3.up, Random.Range(0f, 360f));
+            }
+           
+        }
+    }
+}
