703c3d9966225d387efe9616b42c4bbe7557cbaf
Michael Jack <michaeljack255@gmail.com>
2019-12-21 00:46:56 +0900

    Fixed reload battery

--- Assets/Scripts/Player/FlashLight.cs
+++ Assets/Scripts/Player/FlashLight.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FlashLight : PlayerBase
 {
     private bool isBringTorch;          // Whether player is bring torch
     private bool isLight;               // Whether player is turned on light
+    private bool isReload;              // Whether player is reload battery
     private float batLevel = 50f;          // The amount of battery level.
     private int batCount = 0;              // The amount of battery count.
     private float distance = 15f;  //The distance in float between player and enemy.
@@ -13,6 +15,7 @@ public class FlashLight : PlayerBase
 
     private float timer = 5;              // Timer
     private float currTimer;            //current timer
+    private float reloadTimer = 5;
 
     //Global Variable
     private Transform hand;         // Reference to hand component.
@@ -20,7 +23,7 @@ public class FlashLight : PlayerBase
     private Examines examines;               // Reference to examines component.
     private Light pointLight;                // Reference to light component.
     private AudioManager audioManager;
-
+    private Image reloadUI;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +32,11 @@ public class FlashLight : PlayerBase
         currTimer = timer;
         //light off
         isLight = false;
+        //reload false
+        isReload = false;
+        //Get ReloadUI
+        reloadUI = hud.transform.Find("ReloadUI").GetComponent<Image>();
+
         //find hand component
         hand = this.transform.Find("Hand");
         mainCamera = this.transform.GetComponent<Camera>();
@@ -122,25 +130,59 @@ public class FlashLight : PlayerBase
 
         }
 
+        
+        
+        
+        if (isReload)
+        {
+            reloadTimer -= Time.deltaTime;
+            reloadUI.fillAmount = reloadTimer/5;
+            if (reloadTimer<0)
+            {
+                reloadTimer = 0;
+                reloadUI.fillAmount = 0;
+            }
+        }
+        else if(!isReload)
+        {
+            reloadTimer = 5;
+            reloadUI.fillAmount = reloadTimer/5;
+            reloadUI.gameObject.SetActive(false);
+        }
+        print(reloadTimer);
         /*====================
         *press reload flashlight
         =====================*/
-        if (Input.GetButtonUp("Reload"))
+        if (Input.GetButtonUp("Reload")&& batCount>0 && IsBringTorch)
         {
-            //random reload
-            //Battery 80-100%
-            int randomBat = Random.Range(80, 100);
-            if (batCount > 0)
-            {
-                batCount -= 1;
-                //SetBatLevel to 80-100;
-                this.SetBatLevel = randomBat;
-            }
-            else if (batCount < 0)
-            {
-                batCount = 0;
-            }
+            //give delay 5 second
+            StartCoroutine(ReloadCoroutine(reloadUI));
+            
+            
 
+        }
+    }
+
+    private IEnumerator ReloadCoroutine(Image reloadUI)
+    {
+        isReload = true;
+        reloadUI.gameObject.SetActive(true);
+        //play se
+        audioManager.PlaySE("change_battery");
+        yield return new WaitForSeconds(reloadTimer);
+        //random reload
+        //Battery 80-100%
+        int randomBat = Random.Range(80, 100);
+        if (batCount > 0 && reloadTimer <= 0)
+        {
+            batCount -= 1;
+            //SetBatLevel to 80-100;
+            this.SetBatLevel = randomBat;
+            isReload = false;
+        }
+        else if (batCount < 0)
+        {
+            batCount = 0;
         }
     }
 
